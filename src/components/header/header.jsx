f37ca1007d56acf1e01901cfa1d785f5b0396d1b
Anushavan <anushavan777@gmail.com>
2021-06-25 16:04:32 +0400

    adding lang_icons

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,13 @@
+//COMPONENTS
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import i18n from '../../locales/i18n';
 //CSS
 import './header.css';
+//PICTURE
+import russian from '../../img/png/russian.png';
+import english from '../../img/png/english.png';
 
 const Header = () => {
     const { t } = useTranslation();
@@ -15,27 +19,44 @@ const Header = () => {
     return (
         <>
             <div>
-                <div className="d-flex flex-column align-items-end pt-5 pb-2 ">
-                    <div className="pb-4">
-                        <h4 className="float-right text-white">
-                            +374 (77) 77 33 77
-                        </h4>
-                    </div>
-                    <div className="pb-4">
-                        <a href="/" className="float-right phone_bar">
-                            {t('ЗАКАТЬ ЗВАНОК')}
-                        </a>
-                    </div>
-                    <div>
-                        <Button
-                            className="mr-4"
-                            onClick={() => handleChangeLang('en')}
-                        >
-                            EN
-                        </Button>
-                        <Button onClick={() => handleChangeLang('ru')}>
-                            RU
-                        </Button>
+                <div className="d-flex justify-content-end">
+                    <div className="d-flex align-items-center pt-2 pb-2 ">
+                        <div className="">
+                            <h4 className="float-right text-white">
+                                +374 (77) 77 33 77
+                            </h4>
+                        </div>
+                        <div className="">
+                            <a href="/" className="float-right phone_bar">
+                                {t('ЗАКАТЬ ЗВАНОК')}
+                            </a>
+                        </div>
+                        <div>
+                            {/* <Button
+                                className="mr-2 ml-2"
+                                onClick={() => handleChangeLang('en')}
+                            >
+                                EN
+                            </Button>
+                            <Button onClick={() => handleChangeLang('ru')}>
+                                RU
+                            </Button> */}
+                            <div className="wrapper_lng">
+                                <span
+                                    className="lng"
+                                    onClick={() => handleChangeLang('ru')}
+                                >
+                                    <img src={russian} alt="lng" />
+                                </span>
+                                <span onClick={() => handleChangeLang('en')}>
+                                    <img
+                                        className="lng"
+                                        src={english}
+                                        alt="lng"
+                                    />
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div>
